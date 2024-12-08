abfda62af83319a81568ed6490079975bb4698cf
Sergey Novikov <nsv47nsv36@yandex.ru>
2024-12-08 08:30:25 +0300

    без GIT'a и жизнь не та

---
+++ src/hfbeacon.h
@@ -0,0 +1,47 @@
+/********************************************************************************************
+ * HF multimodes beacon Arduino library
+ * Created 04/10/2014
+ * Christophe Caiveau f4goj@free.fr 
+ * 
+ * Use this library freely
+ *
+ * Instance :
+ *
+ * Functions :
+ *
+ *******************************************************************************************/
+ 
+ 
+#ifndef HFBEACON_H
+#define HFBEACON_H
+#include <Arduino.h>
+#include <AD9833.h>
+
+class HFBEACON
+{
+  public:
+    HFBEACON();
+   
+   void rsidToggle(boolean rsidEnable);
+   void cwTx(long freqCw, char * stringCw, int cwWpm, AD9833*);
+   void pskTx(long freqPsk, char * stringPsk, int modePsk, int baudsPsk);
+   void rttyTx(long freqRtty, char * stringRtty);
+   void hellTx(long freqHell, char * stringHell);
+   void wsprTx(long freqWspr, char * callWsprTx, char * locWsprTx, char * powWsprTx);
+   void wsprEncode(char * callWsprProc, char * locWsprProc, char * powWsprProc);
+   void ddsPower(int powDds, AD9833*);
+   byte wsprSymb[162];
+   int wsprSymbGen;
+			
+  private:
+   void rsidTx(long freqRsid, int modeRsid, AD9833*);
+   void pskIdle(long freqIdle, int baudsIdle);
+   void rttyTxByte (long freqRttyTxbyte, char c);
+   
+   byte parity(unsigned long tempo);
+   byte rsidTxEnable;
+};
+
+extern HFBEACON Beacon;
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <AD9833.h>
+#include <hfbeacon.h>
+#include <avr/pgmspace.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+uint32_t timer_1 = 0;
+
+#define FNC_PIN 4       // Can be any digital IO pin
+
+AD9833 gen(FNC_PIN);       // Defaults to 25MHz internal reference frequency
+
+long freq=10140600;
+
+char txString[] = "NSV";
+
+int freeRam () 
+{
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(4,OUTPUT);
+  // Serial.begin(57600);
+ Serial.print(F("Free RAM : "));
+ Serial.println(freeRam());
+//  DDS.begin(13,8,9);
+//  DDS.calibrate(124998250);
+  gen.Begin();
+  gen.ApplySignal(SQUARE_WAVE,REG0,freq); // SINE_WAVE // SQUARE_WAVE // HALF_SQUARE_WAVE
+  gen.EnableOutput(true);   // Turn ON the output - it defaults to OFF
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Beacon.cwTx(freq, txString, 20, &gen);
+   delay(1000);
+   
+   //Beacon.pskTx(freq, txString, 'B', 31);
+   //delay(2000);
+   
+   //Beacon.rttyTx(freq, txString);
+   //delay(2000);
+   
+   //Beacon.hellTx(freq, txString);
+   //delay(2000);
+   
+   //Beacon.wsprTx(freq, txCall, txLoc, txPow);
+   //delay(10000);
+   
+   Serial.print(F("Free RAM : "));
+   Serial.println(freeRam());
+
+}
